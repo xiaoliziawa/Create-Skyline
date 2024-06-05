8c155e94242baa432984349ca66263eb3283d333
xiaoliziawa <xiaoliziawa@gmail.com>
2024-06-05 14:47:45 +0800

    -

--- kubejs/startup_scripts/Item.js
+++ kubejs/startup_scripts/Item.js
@@ -1,9 +1,10 @@
 StartupEvents.registry('minecraft:item', e => {
     /**
      * 
-     * @param {string} Id 
-     * @param {string} Rarity 
-     * @param {boolean} Glow 
+     * @param {string} Id -注册方块ID
+     * @param {string} Rarity -注册方块稀有度
+     * @param {boolean} Glow -注册方块是否发光
+     * 
     */
     
     let ItemCreate = (Id, Rarity, Glow) => { e.create(Id).rarity(Rarity).glow(Glow) }
