ad0c82f2e8ba93d9f0880e934675932534810b68
0xarch <alanqa-ops@outlook.com>
2024-06-08 22:35:19 +0800

    update@fork: kubejs@startup_scripts: easier functions

--- kubejs/startup_scripts/Block.js
+++ kubejs/startup_scripts/Block.js
@@ -1,36 +1,45 @@
 StartupEvents.registry('block', e => {
-    const ToolType = {
-		sword: 'minecraft:mineable/sword',
-		pickaxe: 'minecraft:mineable/pickaxe',
-		axe: 'minecraft:mineable/axe',
-		shovel: 'minecraft:mineable/shovel',
-		hoe: 'minecraft:mineable/hoe'
-	}
-	// 挖掘等级
-	const MininGlevel = {
-		wooden: 'minecraft:needs_wooden_tool',
-		stone: 'minecraft:needs_stone_tool',
-		iron: 'minecraft:needs_iron_tool',
-		golded: 'minecraft:needs_gold_tool',
-		diamond: 'minecraft:needs_diamond_tool',
-		nether: 'forge:needs_netherite_tool'
+  const ToolType = {
+    sword: 'minecraft:mineable/sword',
+    pickaxe: 'minecraft:mineable/pickaxe',
+    axe: 'minecraft:mineable/axe',
+    shovel: 'minecraft:mineable/shovel',
+    hoe: 'minecraft:mineable/hoe'
+  }
+  // 挖掘等级
+  const MiningLevel = {
+    wooden: 'minecraft:needs_wooden_tool',
+    stone: 'minecraft:needs_stone_tool',
+    iron: 'minecraft:needs_iron_tool',
+    golded: 'minecraft:needs_gold_tool',
+    diamond: 'minecraft:needs_diamond_tool',
+    nether: 'forge:needs_netherite_tool'
+  }
+  /**
+   * 
+   * @param {string} ID -方块ID
+   * @param {string} SoundType -方块破坏&放置声音
+   * @param {number} Hardness -方块硬度
+   * @param {number} Resistance -方块爆炸抗性
+   * @param {string|string[]} Tool -方块破坏需要的工具类型
+   * @param {string} Grade -方块需要的工具材质
+   */
+  let blockAdd = (ID, SoundType, Hardness, Resistance, Tool, Grade = '') => {
+    const block = e.create(ID)
+      .soundType(SoundType)
+      .hardness(Hardness)
+      .resistance(Resistance)
+      .tagBlock(MiningLevel[Grade]);
+
+    if(!!Grade){ // NOTE: 如果 Grade 不传则视为不需要工具 空字符串同样被视为不需要工具
+      block.tagBlock(MiningLevel[Grade]);
+      block.requiresTool(true);
     }
-    /**
-     * 
-     * @param {string} ID -方块ID
-     * @param {string} SoundType -方块破坏&放置声音
-     * @param {number} Hardness -方块硬度
-     * @param {number} ResisTance -方块爆炸抗性
-     * @param {string} Tool -方块破坏需要的工具类型
-     * @param {string} Grade -方块需要的工具材质
-     */
-    let blockAdd = (ID, SoundType, Hardness, ResisTance, Tool, Grade) => {
-        e.create(ID)
-            .soundType(SoundType)
-            .hardness(Hardness)
-            .resistance(ResisTance)
-            .tagBlock(ToolType[Tool])
-            .tagBlock(MininGlevel[Grade])
-            .requiresTool(true)
+
+    if (Array.isArray(Tool)) {
+      Tool.forEach(v => block.tagBlock(ToolType[Tool]));
+    } else {
+      block.tagBlock(ToolType[Tool])
+    }
   }
 })
--- kubejs/startup_scripts/Item.js
+++ kubejs/startup_scripts/Item.js
@@ -2,19 +2,19 @@ StartupEvents.registry('minecraft:item', e => {
     /**
      * 
      * @param {string} Id -注册方块ID
-     * @param {string} Rarity -注册方块稀有度
-     * @param {boolean} Glow -注册方块是否发光
+     * @param {string} Rarity -注册方块稀有度 默认 `common`
+     * @param {boolean} Glow -注册方块是否发光 默认不发光
      * 
     */
-    let ItemCreate = (Id, Rarity, Glow) => { e.create(Id).rarity(Rarity).glow(Glow) }
-    
+    let ItemCreate = (Id, Rarity = 'common', Glow = false) => e.create(Id).rarity(Rarity).glow(Glow);
+
     let ItemAdd = [
         ['stone_grain', 'common', false], //石子
         ['stone_rod', 'common', false] //石棍
-    ]
+    ];
 
     ItemAdd.forEach(([ids, rarity, glow]) => [
-        ItemCreate(ids,rarity,glow)
-    ])
+        ItemCreate(ids, rarity, glow)
+    ]);
 
 })
