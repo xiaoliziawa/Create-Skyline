97f96e555e2078f2438a4b7220332ae23937aa2b
0xarch <alanqa-ops@outlook.com>
2024-06-08 22:54:49 +0800

    update@fork: kubejs@startup_scripts: easier functions

--- kubejs/startup_scripts/Item.js
+++ kubejs/startup_scripts/Item.js
@@ -2,20 +2,29 @@ StartupEvents.registry("item", (event) => {
   /**
    *
    * @param {string} id -注册方块ID
-   * @param {string} rarity -注册方块稀有度
-   * @param {boolean} glow -注册方块是否发光
+   * @param {string} rarity -注册方块稀有度 默认`common`
+   * @param {boolean} glow -注册方块是否发光 默认`false`
    *
    */
-  function customItemBuilder(id, rarity, glow) {
+  function customItemBuilder(id, rarity = 'common', glow = false) {
     event.create(id).rarity(rarity).glow(glow);
   }
 
+  /**
+   * @type {string|[string,string?,boolean?]}
+   */
   let itemData = [
     ["stone_grain", "common", false], //石子
-    ["stone_rod", "common", false], //石棍
+    // ["stone_rod", "common", false], //石棍
+    "stone_rod" // 简化
   ];
 
-  itemData.forEach(([ids, rarity, glow]) => {
-    customItemBuilder(ids, rarity, glow);
+  itemData.forEach((data) => {
+    if(Array.isArray(data)){
+      let [ids, rarity, glow] = data;
+      customItemBuilder(ids, rarity, glow);
+    }else {
+      customItemBuilder(data);
+    }
   });
 });
